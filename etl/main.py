6dbeff81aa719d73bfc64638857b5fa4134cfe3c
KellanSelig <kselig@hopper.com>
2021-08-24 22:47:32 -0400

    Refactor & Clean ETL, and add CLI

--- etl/main.py
+++ etl/main.py
@@ -1,71 +1,26 @@
-from google.cloud import bigquery
-import requests
-from typing import List, Any, Dict
+import argparse
 import logging
-import time
 
-BASEURL = "https://proclubs.ea.com/api/nhl"
-HEADERS = {"referer": 'www.ea.com'}
+from src.models import Records
+from src.proclubs import ProClubs
+from util import consume
 
-platforms = ('ps4', 'xboxone')
-match_types = (
-    'gameType5',  # regular match
-    'gameType10',  # playoff match
-    'club_private'  # private match
-)
-
-session = requests.session()
+logging.basicConfig(level=logging.DEBUG)
 
 
-def request_(url: str, params=None, **kwargs) -> Any:
-    logging.info(params)
-    resp = session.get(url, headers=HEADERS, params=params, **kwargs)
-    resp.raise_for_status()
-    return resp.json()
+def main(to_bq: bool = False):
+    if to_bq:  # FIXME -> data returned from proclubs currently is not loadable
+        raise NotImplementedError("Currently only loading to JSON is supported")
+
+    proclubs = ProClubs()
+    with Records() as records:
+        logging.info("Starting requests for match data")
+        consume(records.extend(matches) for matches in proclubs.get_all_matches())
+        getattr(records, "to_bigquery" if to_bq else "to_json")()
 
 
-def get_clubs(platform) -> List[int]:
-    url = f'{BASEURL}/seasonRankLeaderboard'
-    params = {'platform': platform}
-    resp = request_(url, params=params)
-    return [r['clubInfo']['clubId'] for r in resp]
-
-
-def request_match_data(platform: str, match_type: str, club_id: int, max_result: int = 1) -> List[Dict]:
-    params = {'platform': platform, 'matchType': match_type, 'maxResultCount': max_result, 'clubIds': club_id}
-    url = f'{BASEURL}/clubs/matches'
-    try:
-        return request_(url, params=params)
-    except requests.exceptions.HTTPError as e:
-        if e.response.status_code == 500 and max_result > 1:
-            max_result = max(1, max_result - 5)
-            time.sleep(2)
-            return request_match_data(platform, match_type, club_id, max_result)
-        else:
-            raise e
-
-
-def main():
-    """Add note about those clubs! """
-    data = []
-    for platform in platforms:
-        clubs = get_clubs(platform)
-        for club_id in clubs:
-            data.extend(request_match_data(platform, match_type, club_id) for match_type in match_types)
-
-
-    # TODO setup table, temp dataset. upsert method
-    # bq_client = bigquery.Client()
-    # table: bigquery.Table = bq_client.get_table("tablename")
-    # job_config = bigquery.LoadJobConfig(
-    #     schema=table.schema,
-    #     ignore_unknown_values=True,
-    #     write_disposition=bigquery.WriteDisposition.WRITE_APPEND
-    # )
-    # bq_client.load_table_from_json(data, "destingation", job_config=job_config)
-
-
-if __name__ == '__main__':
-    # main()
-    m = request_match_data(platform='ps4', match_type='gameType10', club_id=38415, max_result=85)
-    print(m)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-bq", "--to_bigquery", action="store_true", default=False)
+    args = parser.parse_args()
+    main(args.to_bigquery)
---
+++ etl/src/proclubs.py
@@ -0,0 +1,57 @@
+import logging
+import time
+from typing import List, Any, Dict
+
+from requests import Session
+from requests.adapters import HTTPAdapter
+from requests.exceptions import HTTPError
+from requests.packages.urllib3.util.retry import Retry
+
+BASEURL = "https://proclubs.ea.com/api/nhl"
+HEADERS = {"referer": "www.ea.com"}
+PLATFORMS = ("ps4", "xboxone")
+MATCH_TYPES = ("gameType5", "gameType10", "club_private")
+
+
+class ProClubs:
+    """Request Data from proclubs api"""
+
+    def __init__(self):
+        adapter = HTTPAdapter(max_retries=Retry(total=3, backoff_factor=2))
+        session = Session()
+        session.mount("https://", adapter)
+        session.mount("http://", adapter)
+
+        self.session = session
+
+    def _request(self, url: str, params=None, **kwargs) -> Any:
+        logging.info(params)
+        resp = self.session.get(url, headers=HEADERS, params=params, **kwargs)
+        resp.raise_for_status()
+        return resp.json()
+
+    def request_club_list(self, platform) -> List[int]:
+        url = f"{BASEURL}/seasonRankLeaderboard"
+        resp = self._request(url, params={"platform": platform})
+        return [r["clubInfo"]["clubId"] for r in resp]
+
+    def request_match(self, platform: str, match_type: str, club_id: int, max_result: int = 50) -> List[Dict]:
+        params = {"platform": platform, "matchType": match_type, "maxResultCount": max_result, "clubIds": club_id}
+        url = f"{BASEURL}/clubs/matches"
+        try:
+            return self._request(url, params=params)
+        except HTTPError as e:
+            if e.response.status_code == 500 and max_result > 1:
+                logging.warning(f"Match data did not exist for max_results = {max_result}. Retrying with fewer results")
+                max_result = max(1, max_result - 5)
+                time.sleep(1)
+                return self.request_match(platform, match_type, club_id, max_result)
+            else:
+                raise e
+
+    def get_all_matches(self):
+        for platform in PLATFORMS:
+            clubs = self.request_club_list(platform)
+            for club_id in clubs:
+                for match_type in MATCH_TYPES:
+                    yield self.request_match(platform, match_type, club_id)

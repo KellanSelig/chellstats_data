47bef6dc4c45f5e46d4d9fd2071e039166038ea4
KellanSelig <kselig@hopper.com>
2021-09-16 20:38:10 -0400

    Add support for writing to BQ

--- etl/src/models.py
+++ etl/src/models.py
@@ -4,11 +4,12 @@ from datetime import datetime
 from typing import List, Dict, Union, Iterable
 
 from google.cloud import bigquery
+from google import auth
 from pydantic import BaseModel
 
 
 class Records(BaseModel):
-    data: List = []
+    __data: List = []
     created_at_dt: datetime = datetime.now()
 
     def __enter__(self):
@@ -22,7 +23,32 @@ class Records(BaseModel):
             records = [records]
         for r in records:
             r["created_at_dt"] = self.created_at_dt.isoformat()
-            self.data.append(r)
+            self.__data.append(self.transform(r))
+
+    @staticmethod
+    def transform(r):
+        """
+        Transform records into valid format for loading into relational database.
+
+        The data is sent with some values that cannot be directly loaded into a relational database
+        An example is the clubs are sent with a key of the club_id. Relational databases can't take a key that is
+        an integer.
+
+        :param r: Single record returned by proclubs API.
+        """
+        r["clubs"] = [club | {"club": club_id} for club_id, club in r["clubs"].items()]
+        r["aggregate"] = [club | {"club": club_id} for club_id, club in r["aggregate"].items()]
+        r["players"] = [
+            player | {"club": club_id, "player": player_id}
+            for club_id, players in r["players"].items()
+            for player_id, player in players.items()
+        ]
+        return r
+
+
+    @property
+    def data(self):
+        return self.__data
 
     def to_json(self, path="test_data.json"):
         logging.info(f"Writing records to {path}")
@@ -33,7 +59,8 @@ class Records(BaseModel):
 
     def to_bq(self) -> None:
         bq_client = bigquery.Client()
-        table: bigquery.Table = bq_client.get_table("tablename")
+        project_id = auth.default()[1]
+        table: bigquery.Table = bq_client.get_table(f"{project_id}.records.matches")
         job_config = bigquery.LoadJobConfig(
             schema=table.schema,
             ignore_unknown_values=True,

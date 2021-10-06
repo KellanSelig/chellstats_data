3b165647dc4ecdc52797167e9832b6d3542be118
KellanSelig <kselig@hopper.com>
2021-10-05 21:47:03 -0400

    Add code quality checks

---
+++ run_checks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
+
+echo "Running Code Quality Checks"
+
+echo "Formatting files..."
+isort .
+black .
+echo
+
+echo "Checking Style..."
+flake8
+echo
+
+echo "Checking Type Annotations..."
+mypy .
+echo
+
+echo "Running tests..."
+# etl tests
+PYTHONPATH=etl/app pytest ./tests/etl -s
